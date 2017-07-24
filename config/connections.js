ea488a7d741c2bbd149e2d2c34a89934598ca8fb
hockey4life63 <Amelancon68@gmail.com>
2017-07-24 14:14:08 -0700

    fixed connection bug

--- config/connections.js
+++ config/connections.js
@@ -1,21 +1,22 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-const key = require("./key")
-const connection = mysql.createConnection(key);
+const key =("./key")
+let connection;
 
-// Make connection.
 if(process.env.JAWSDB_URL){
-  connection.createConnection(process.env.JAWSDB_URL)
-}else{
-  connection.connect(function(err) {
+
+ connection= mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection(key)
+}
+// Make connection.
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
-}
-
 
 // Export connection for our ORM to use.
 module.exports = connection;
